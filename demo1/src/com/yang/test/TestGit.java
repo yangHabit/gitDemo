3156e516e42dee4e753485112f3849802d882f6d
yang <2280539387@qq.com>
2022-06-17 17:16:48 +0800

    尝试进行第一次更改

---
+++ demo1/src/com/yang/test/TestGit.java
@@ -0,0 +1,12 @@
+package com.yang.test;
+
+/**
+ * @author: YangJiao
+ * @email 1320259466@qq.com
+ * @date: 2022/6/17
+ * @time: 17:14
+ * @fuction: about the role of class.
+ */
+public class TestGit {
+
+}
