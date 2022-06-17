9b34c1c15bdf13f5ffacd4838a612c3209c3949a
yang <2280539387@qq.com>
2022-06-17 17:33:30 +0800

    尝试进行第4次更改

--- demo1/src/com/yang/test/TestGit.java
+++ demo1/src/com/yang/test/TestGit.java
@@ -1,14 +1,8 @@
 package com.yang.test;
 
-/**
- * @author: YangJiao
- * @email 1320259466@qq.com
- * @date: 2022/6/17
- * @time: 17:14
- * @fuction: about the role of class.
- */
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("git hello");
+        System.out.println("1111");
     }
 }
